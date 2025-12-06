5b3b6924a53bc84ed4a22e2a8c18677ba882c563
mahmoudediem <mahmoudediem1@gmail.com>
2025-12-06 20:54:59 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,6 +5,6 @@ setup(
     version="0.1.0",
     packages=find_packages(),
     install_requires=[
-        "git+https://github.com/mahmoudediem/shareddep.git@v3.0.0#egg=shareddep"
+        "shareddep"   # Only the package name goes here
     ],
 )
